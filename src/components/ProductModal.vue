4166d3e152851784fd7963b2f48940218278b092
Ase444ka <istomina.asia@yandex.ru>
2024-11-14 22:23:33 +0300

    add cart-page

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -69,7 +69,7 @@ export default {
 </script>
 <style scoped lang="scss">
 .modal {
-  display: none;
+  display: block;
   position: fixed;
   bottom: 0;
   top: 0;
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,36 +1,82 @@
 <template>
-   <div>
-     CartPage
-   </div>
+  <main>
+    <section class="cart">
+      <h2>корзина</h2>
+      <div class="row header">
+        <div>Товар</div>
+        <div>Цена, руб</div>
+        <div>Кол-во</div>
+        <div>Сумма, руб</div>
+      </div>
+      <div class="row">
+        <div>Доска с листьями</div>
+        <div>1000</div>
+        <div>1</div>
+        <div>1000</div>
+        <div><button class="remove"></button></div>
+      </div>
+      <div class="row">
+        <div>Веселый куб</div>
+        <div>5000</div>
+        <div>1</div>
+        <div>500</div>
+        <div><button class="remove"></button></div>
+      </div>
+      <div class="row footer">
+        <div>Итог</div>
+        <div>5000</div>
+        <div>2</div>
+        <div>6000</div>
+      </div>
+    </section>
+    <ProductModal />
+  </main>
 </template>
 <script>
 export default {
-   name: 'CartPage',
-   components: {
-     
-   },
-   mixins: [],
-   props: {
-     
-   },
-   data() {
-     return {
-       
-     }
-   },
-   computed: {
-     
-   },
-   watch: {
-     
-   },
-   mounted() {
-     
-   },
-   methods: {
-     
-   }
+  name: 'CartPage',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
 };
 </script>
-<style lang='' scoped>
+<style lang="scss" scoped>
+.row {
+  display: grid;
+  grid-template-columns: minmax(max-content, 300px) 1fr 1fr 1fr 30px;
+  border-bottom: 2px solid var(--primary);
+  border-left: 2px solid var(--primary);
+  border-right: 2px solid var(--primary);
+  padding: 20px;
+  font-weight: medium;
+  font-size: 22px;
+}
+.header {
+  border-top: 2px solid var(--primary);
+  
+}
+.header, .footer {
+  * {
+    font-weight: bold;
+  }
+}
+
+.remove {
+  outline: none;
+  border: none;
+  background: url(@/assets/trash.svg);
+  background-color: var(--white);
+  width: 18px;
+  height: 18px;
+
+  background-size: contain;
+  background-repeat: no-repeat;
+}
 </style>
